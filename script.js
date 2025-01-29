2bb16a1c9a46fb59e065a0e16bf2a154fb5fcd2f
Sergey Stepantsov <mitrotskiy@yandex.ru>
2025-01-29 19:30:52 +0700

    gitignore or something i dunno honestly

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ import * as PIXI from "./pixi.mjs";
 
 const app = new PIXI.Application();
 await app.init({
-    width: 1920,
-    height: 1080,
+    width: 192,
+    height: 108,
     background: "#161218",
     useBackBuffer: true,
     autoDensity: true,
 }); // #A5C5B5
 document.body.appendChild(app.canvas);
+
+const circle = new PIXI.Graphics().circle(app.screen.width / 2, app.screen.height / 2, 40).fill(0xFF0000);
+app.stage.addChild(circle);
